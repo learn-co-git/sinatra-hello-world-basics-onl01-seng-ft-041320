c047ed51bf2f902185e29649331ee0fc8c3ec1a5
Neil Empey <argus.two.2@gmail.com>
2020-05-18 07:40:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,10 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   def get   
-    GET '/' do 
-      "Hello, World!"
-    end
+     get '/' do
+    "Hello, World!"
+  end
+
   end
 
 end
